885fd5997b9e19a6e4106753b7b0c956b9dd1352
Caleb Owens <caleb@gitbutler.com>
2025-05-05 18:39:32 +0200

    Added conditions before running the removals

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
 cd /home/ec2-user/thewistfulfox
-docker stop foxy
+
+# Stop the container if it's running
+if [ $(docker ps -q -f name=foxy) ]; then
+    docker stop foxy
+fi
+
+# Remove the container if it exists
+if [ $(docker ps -a -q -f name=foxy) ]; then
+    docker rm foxy
+fi
